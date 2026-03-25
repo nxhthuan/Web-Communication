b860773f737ccfe25797b523028ab7bd703a9a52
nguyxuan <nguyxuan@arcada.fi>
2026-03-25 15:00:35 +0200

    detect ip

--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
-    return { "msg": "Bonjour!" }
+    return {"message": "Bonjour Anthony"}
 
 
-@app.get("/items/{id}")
-def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
+@app.get("/api/ip")
+def get_ip(request: Request):
+    client_ip = request.client.host
+    return {"ip": client_ip}
+
+
+@app.get("/ip", response_class=HTMLResponse)
+def get_ip_html(request: Request):
+    client_ip = request.client.host
+    return f"<h1>Your public IP is {client_ip}</h1>"
